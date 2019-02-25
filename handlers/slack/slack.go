ab90cf0e52e45131e35b2e89d91010cf9361b831
uthng <u.nguyenthanh@gmail.com>
2019-02-25 01:01:37 +0100

    feat(handler/slack): Add functions formatting slack message for different cases: simple (print, printf, println), semi or full structured log (#24)
    Closes #24

--- handlers/slack/slack.go
+++ handlers/slack/slack.go
@@ -2,10 +2,11 @@ package slack
 
 import (
 	"fmt"
+	"strings"
 	"sync"
 	//"time"
 
-	"github.com/spf13/cast"
+	//"github.com/spf13/cast"
 	"github.com/uthng/slack"
 
 	log "github.com/uthng/golog"
@@ -20,7 +21,7 @@ type handler struct {
 	iconEmoji string
 	iconURL   string
 	channel   string
-	msgTitle  string
+	title     string
 
 	mutex sync.Mutex
 }
@@ -29,15 +30,15 @@ var slackWebhookURL = "https://hooks.slack.com/services/"
 
 // Color Map following levels
 var colors = map[int]string{
-	log.FATAL: "#fffff",
-	log.ERROR: "#fffff",
-	log.WARN:  "#fffff",
-	log.INFO:  "#fffff",
-	log.DEBUG: "#fffff",
+	log.FATAL: "#cc0000",
+	log.ERROR: "danger",
+	log.WARN:  "warning",
+	log.INFO:  "good",
+	log.DEBUG: "#7e7e7c",
 }
 
 // New creates a new slack handler
-func New(token, username, iconEmoji, iconURL, channel, msgTitle string, verbose int) log.Handler {
+func New(token, username, iconEmoji, iconURL, channel, title string, verbose int) log.Handler {
 	s := &handler{
 		verbose:   verbose,
 		token:     token,
@@ -45,7 +46,7 @@ func New(token, username, iconEmoji, iconURL, channel, msgTitle string, verbose 
 		iconEmoji: iconEmoji,
 		iconURL:   iconURL,
 		channel:   channel,
-		msgTitle:  msgTitle,
+		title:     title,
 	}
 
 	return s
@@ -64,7 +65,12 @@ func (h *handler) PrintMsg(p int, l *log.Logger, level int, fields log.Fields) e
 		case log.PRINT, log.PRINTF, log.PRINTLN:
 			return h.printWebhook(level, fields)
 		case log.PRINTW:
-			break
+			flag := l.GetFlags()
+			if flag&log.FFULLSTRUCTUREDLOG != 0 {
+				return h.printwWebhook(level, fields, true)
+			}
+
+			return h.printwWebhook(level, fields, false)
 		}
 	}
 
@@ -72,6 +78,61 @@ func (h *handler) PrintMsg(p int, l *log.Logger, level int, fields log.Fields) e
 }
 
 func (h *handler) printWebhook(level int, fields log.Fields) error {
+	var webhookMsg *slack.WebhookMessage
+	var attachment *slack.Attachment
+	var prefix string
+
+	webhookMsg = initWebhookMessage(h, level)
+	attachment = &webhookMsg.Attachments[0]
+
+	for _, p := range fields.Prefix {
+		val := p.Value
+
+		if p.Key == "level" {
+			val = "*" + val + "*:"
+		}
+		prefix = prefix + val + " "
+	}
+
+	var message string
+	message = fmt.Sprint(fields.Log[0].Value)
+
+	attachment.Text = prefix + message
+
+	return slack.PostWebhook(slackWebhookURL+h.token, webhookMsg)
+}
+
+func (h *handler) printwWebhook(level int, fields log.Fields, full bool) error {
+	var webhookMsg *slack.WebhookMessage
+	var attachment *slack.Attachment
+
+	webhookMsg = initWebhookMessage(h, level)
+	attachment = &webhookMsg.Attachments[0]
+
+	for _, p := range fields.Log {
+		if p.Key == "msg" && !full {
+			attachment.Text = p.Value
+		} else {
+			field := slack.AttachmentField{
+				Value: "*" + strings.Title(p.Key) + "*: " + p.Value,
+			}
+
+			attachment.Fields = append(attachment.Fields, field)
+		}
+	}
+
+	for _, p := range fields.Prefix {
+		field := slack.AttachmentField{
+			Value: "*" + strings.Title(p.Key) + "*: " + p.Value,
+		}
+
+		attachment.Fields = append(attachment.Fields, field)
+	}
+
+	return slack.PostWebhook(slackWebhookURL+h.token, webhookMsg)
+}
+
+func initWebhookMessage(h *handler, level int) *slack.WebhookMessage {
 	webhookMsg := &slack.WebhookMessage{
 		Username:  h.username,
 		IconEmoji: h.iconEmoji,
@@ -81,24 +142,14 @@ func (h *handler) printWebhook(level int, fields log.Fields) error {
 
 	attachment := slack.Attachment{}
 
-	if h.msgTitle != "" {
-		attachment.Title = h.msgTitle
+	if h.title != "" {
+		attachment.Title = h.title
 	}
 
 	attachment.Color = colors[level]
 	attachment.MarkdownIn = append(attachment.MarkdownIn, "text", "fields")
 
-	var prefix string
-	for _, p := range fields.Prefix {
-		prefix = prefix + cast.ToString(p.Value) + " "
-	}
-
-	var message string
-	message = fmt.Sprint(fields.Log[0].Value)
-
-	attachment.Text = prefix + message
-
 	webhookMsg.Attachments = append(webhookMsg.Attachments, attachment)
 
-	return slack.PostWebhook(slackWebhookURL+h.token, webhookMsg)
+	return webhookMsg
 }
